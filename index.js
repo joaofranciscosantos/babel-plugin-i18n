9900f36f310ce69bcf1d4f68494917d5f6911c8a
joaofranciscosantos <joao.francis.santos@gmail.com>
2019-01-20 20:53:56 +0100

    Add more tests

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
 const {readFileSync} = require('fs');
 const {resolve} = require('path');
+const src = require('./src');
+
+const readFile = source => {
+  try {
+    return JSON.parse(readFileSync(resolve(source || '.dictionary.json'), 'utf8'));
+  } catch (e) {
+    throw new Error(`file not found: ${e.path}`);
+  }
+};
 
 module.exports = (context, input = {}) => {
-  const targetFilename = input.source || '.dictionary.json';
+  const dictionary = readFile(input.source);
   const targetFunctionName = input.target || 'i18n';
   const targetLanguage = input.language || 'en';
-  const dictionary = JSON.parse(readFileSync(resolve(targetFilename), 'utf8'));
   return {
     visitor: {
       CallExpression: {
-        enter: path => {
-          if (path.node.callee.type === 'Identifier') {
-            const calleeName = path.node.callee.name.toLowerCase();
-            const args = path.node.arguments;
-            if (calleeName === targetFunctionName) {
-              const keywordType = args && args[0] && args[0].type;
-              const keyword = args && args[0] && args[0].value;
-              const language = (args && args[1] && args[1].value) || targetLanguage;
-              if ((keywordType !== 'StringLiteral') ||
-                (dictionary[keyword] === undefined) ||
-                (dictionary[keyword][language] === undefined)) {
-                if (typeof keyword === 'string') {
-                  path.replaceWithSourceString(`"${keyword}"`);
-                  return;
-                } else {
-                  path.replaceWithSourceString(keyword === undefined ? null : keyword);
-                  return;
-                }
-
-              }
-              path.replaceWithSourceString(`"${dictionary[keyword][language]}"`);
-            }
-          }
-        }
+        enter: path => src(path, { targetFunctionName, targetLanguage, dictionary })
       }
     }
   }
---
+++ src/index.js
@@ -0,0 +1,23 @@
+module.exports = (path, {targetFunctionName, targetLanguage, dictionary}) => {
+  if (path.node.callee.type === 'Identifier') {
+    const calleeName = path.node.callee.name.toLowerCase();
+    const args = path.node.arguments;
+    if (calleeName === targetFunctionName) {
+      const keywordType = args && args[0] && args[0].type;
+      const keyword = args && args[0] && args[0].value;
+      const language = (args && args[1] && args[1].value) || targetLanguage;
+      if ((keywordType !== 'StringLiteral') ||
+        (dictionary[keyword] === undefined) ||
+        (dictionary[keyword][language] === undefined)) {
+        if (typeof keyword === 'string') {
+          path.replaceWithSourceString(`"${keyword}"`);
+          return;
+        } else {
+          path.replaceWithSourceString(keyword === undefined ? null : keyword);
+          return;
+        }
+      }
+      path.replaceWithSourceString(`"${dictionary[keyword][language]}"`);
+    }
+  }
+};
