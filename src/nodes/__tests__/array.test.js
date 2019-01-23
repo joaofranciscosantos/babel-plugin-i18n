c7453370203f802a724ece50dd99282cb2f9feeb
joaofranciscosantos <joao.francis.santos@gmail.com>
2019-01-23 21:36:16 +0100

    Reorganize test folders

--- src/nodes/__tests__/array.test.js
+++ src/nodes/__tests__/array.test.js
@@ -1,10 +1,10 @@
 const {join} = require('path');
 const {transform} = require('@babel/core');
-const dictionary = require('./.dictionary.json');
-const plugin = require('../index.js');
+const dictionary = require('../../__tests__/.dictionary.json');
+const plugin = require('../../../index.js');
 
 describe('When using babel-plugin-i18n module', () => {
-  const source = join(__dirname, '.dictionary.json');
+  const source = join(__dirname, '..', '..', '__tests__', '.dictionary.json');
   it('', done => {
     const {code} = transform('i18n(["nadar", "bolos"])', { plugins: [() => plugin(null, {source})] });
     expect(code).toBe(`["${dictionary.nadar.en}", "${dictionary.bolos.en}"];`);
--- src/nodes/__tests__/object.test.js
+++ src/nodes/__tests__/object.test.js
@@ -1,10 +1,10 @@
 const {join} = require('path');
 const {transform} = require('@babel/core');
-const dictionary = require('./.dictionary.json');
-const plugin = require('../index');
+const dictionary = require('../../__tests__/.dictionary.json');
+const plugin = require('../../..');
 
 describe('When using babel-plugin-i18n module', () => {
-  const source = join(__dirname, '.dictionary.json');
+  const source = join(__dirname, '..', '..', '__tests__', '.dictionary.json');
   it('', done => {
     const {code} = transform('i18n({"a": "bolas", "b": "3"})', { plugins: [() => plugin(null, {source})] });
     expect(code).toBe(`"${dictionary.bolos.en}";`);
