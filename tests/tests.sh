3abf3d5a09a091a0bd9a0334a33204a0abbebda9
joaofranciscosantos <joao.francis.santos@gmail.com>
2019-07-21 10:52:45 +0200

    Update test script

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-OUT_FOLDER=tests/out
-mkdir -p ${OUT_FOLDER}
+mkdir -p tests/out
 
 FILE_REGEX=$1*
-if [[ "${FILE_REGEX}" == "" ]]; then
+if [[ "$FILE_REGEX" == "" ]]; then
 	FILE_REGEX=*
 fi
 
-echo tests/in/${FILE_REGEX}
+cd tests/in
+echo ${FILE_REGEX}
 
-for filename in tests/in/${FILE_REGEX}; do
-	OUT=${OUT_FOLDER}/${filename:9}
-	EXPECTED=tests/expected/${filename:9}
+for filename in ${FILE_REGEX}; do
+	OUT=../out/${filename}
+	EXPECTED=../expected/${filename}
 	node ${filename} > ${OUT} 2>&1 | grep -i SomeError
-	if [[ -f ${EXPECTED} && -f ${OUT} ]]; then
-		diff ${EXPECTED} ${OUT}
-		if [[ $? -eq 0 ]]; then
-			echo -e "\e[32mOK\e[0m  ${filename}"
-	  else
-	    echo -e "\e[31mNOK\e[0m ${filename}"
-	  fi
-	else
-		echo -e "\e[31mNOK\e[0m ${EXPECTED} or ${OUT} not found"
-  fi
+	diff ${EXPECTED} ${OUT}
+	if [ $? -eq 0 ]; then
+		echo -e "\e[32mOK\e[0m  ${filename}"
+	 else
+	   echo -e "\e[31mNOK\e[0m ${filename}"
+	fi
 done
