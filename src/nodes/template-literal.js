cacbcd12e27465b30888ab560522402099ee7d14
joaofranciscosantos <joao.francis.santos@gmail.com>
2019-07-29 19:51:10 +0200

    Bump version

--- src/nodes/template-literal.js
+++ src/nodes/template-literal.js
@@ -4,11 +4,11 @@ module.exports = (target, args, dictionary, lang, operations) => {
   const expressions = args.expressions[0];
   let leftExpr = '';
   let rightExpr = '';
-  args.quasis && args.quasis.forEach(el => {
-    if (el.tail) {
-      return rightExpr += operations(target, el, dictionary, lang, operations);
+  args.quasis && args.quasis.forEach(expr => {
+    if (expr.tail) {
+      return rightExpr += operations(target, expr, dictionary, lang, operations);
     }
-    return leftExpr += operations(target, el, dictionary, lang, operations);
+    return leftExpr += operations(target, expr, dictionary, lang, operations);
   });
 
   const expr = expressions && operations(target, expressions, dictionary, lang, operations) || '';
